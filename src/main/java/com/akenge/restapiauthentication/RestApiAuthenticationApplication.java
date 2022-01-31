e61f278e7ae0c98033d8eadc7914cd37c932c6ae
wilfriedpapt89 <wilfriedpapt@gmail.com>
2022-01-31 22:40:06 +0200

    Initial Commit

---
+++ src/main/java/com/akenge/restapiauthentication/RestApiAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.akenge.restapiauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akenge/restapiauthentication/RestApiAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akenge.restapiauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
